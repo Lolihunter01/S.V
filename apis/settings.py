1715f6b37741136027a07ca41f8c56e3c1fda94a
LoliHutner01 <rafibinsayeed2323@gmail.com>
2022-11-09 21:44:22 +0600

    rew

--- apis/settings.py
+++ apis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+0vz=xyh3douw49(a*$zrehn-67z_wpbsxt56ja2bnv$cyu!g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
